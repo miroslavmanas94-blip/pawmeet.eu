28a2297253aff55c7de0bf1d32ae85187dcdba37
miroslavmanas94-blip <miroslavmanas94@gmail.com>
2026-09-07 14:43:55 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+ { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { createClient } from '@/utils/supabase/client'
 import Link from 'next/link'
